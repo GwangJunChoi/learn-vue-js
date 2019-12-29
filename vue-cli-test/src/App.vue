94bf9c5f326df53426e87152566ad8aaba92ecd9
GwangJunChoi <namsookill@gmail.com>
2019-12-29 15:53:17 +0900

    learn-test

---
+++ vue-cli-test/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <app-header v-bind:propsdata="str"
+            v-on:renew="renewStr"></app-header>
+</template>
+
+<script>
+import AppHeader from './components/AppHedaer.vue'
+/* 싱글파일 컴포넌트에서는 사용 x 재사용성을 위해
+ new VUE({
+   data : {
+     
+   }
+ });
+*/
+export default {
+  data : function(){
+    return {
+      str : 'HeaderPropsz'
+    }
+  },
+  components : {
+    'app-header' : AppHeader
+  },
+  methods : {
+    renewStr : function(){
+      this.str = 'test';
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
